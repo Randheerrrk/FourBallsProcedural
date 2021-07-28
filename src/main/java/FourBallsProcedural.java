ddc67aca361afbbe47cbbbe155919b74ffb589c9
Randheer Ramesh K <randheerrrk@gmail.com>
2021-07-28 19:35:20 +0530

    Update FourBallsProcedural.java

--- src/main/java/FourBallsProcedural.java
+++ src/main/java/FourBallsProcedural.java
@@ -7,6 +7,7 @@ public class FourBallsProcedural extends PApplet{
     public static final int C = 10;
     public static final int OFFSET = HEIGHT / 5;
     static int frame = 0;
+    
     public static void main(String[] args) {
         PApplet.main("FourBallsProcedural", args);
     }
@@ -22,10 +23,13 @@ public class FourBallsProcedural extends PApplet{
         for(int i=1; i<=4; i++) ellipse(getX(i), getY(i), C, C);
         frame++;
     }
+    
     private int getX(int position) {
         return position * frame;
     }
+    
     private int getY(int position) {
         return position * OFFSET;
     }
+    
 }
